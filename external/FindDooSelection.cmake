44af968845059a8ecdcdcfe4c19e42894fe78317
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-11-09 14:03:48 +0000

    more files and structure

---
+++ external/FindDooSelection.cmake
@@ -0,0 +1,14 @@
+MESSAGE(STATUS "Looking for DooSelection...")
+
+SET(DooSelection_FOUND FALSE)
+
+IF(IS_DIRECTORY $ENV{DOOSELECTIONSYS})
+  SET(DooSelection_BASE $ENV{DOOSELECTIONSYS})
+  SET(DooSelection_FOUND TRUE)
+  SET(DooSelection_INCLUDES "${DooSelection_BASE}/include")
+  SET(DooSelection_INCLUDE_DIR "${DooSelection_BASE}/include")
+  SET(DooSelection_LIBRARY_DIR "${DooSelection_BASE}/lib")
+  SET(DooSelection_LIBRARIES "-ldsReducer")
+  #SET(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${DooSelection_LIBRARY_DIR})
+  MESSAGE(STATUS "Looking for DooSelection... - found at ${DooSelection_BASE}")
+ENDIF(IS_DIRECTORY $ENV{DOOSELECTIONSYS})
