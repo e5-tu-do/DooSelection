457e776e1e955809fef2ebad781d1fc278d3116f
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-11-09 13:58:27 +0000

    doxygen

--- doc/doxygen_doc.h
+++ doc/doxygen_doc.h
@@ -4,38 +4,37 @@
  *
  * @section mp_introduction Introduction
  *
- * DooCore is a ROOT-/RooFit-/Boost-based framework providing central base 
- * functionality which is needed in nearly all ROOT/RooFit applications in high
- * energy physics analyses.
+ * DooSelection is a ROOT-/TMVA-/RooFit-/Boost-based framework providing central
+ * selection functionality which is useful in high energy physics analyses.
  *
  * @section mp_simplesetup Simple setup via global DooSoftware installation
  * 
- * On all machines with a DooSoftware installation setup of DooCore is very 
+ * On all machines with a DooSoftware installation setup of DooSelection is very
  * simple. In this case you can skip the next building step. Just call 
  * dooSoftwareSetup after setting up LHCb software and DaVinci/ROOT and the
  * DooSoftware magic will do the rest.
  * 
  * @section mp_installation Manual Installation
  *
- * The intended usage of DooCore is as a framework and therefore @a not to 
- * build custom user code inside of DooCore. However, DooCore can be easily 
- * installed and interfaced from other external projects. FindDooCore CMake 
- * modules are supplied which allow easy integration of DooCore in any other 
+ * The intended usage of DooSelection is as a framework and therefore @a not to
+ * build custom user code inside of DooSelection. However, DooSelection can be easily 
+ * installed and interfaced from other external projects. FindDooSelection CMake 
+ * modules are supplied which allow easy integration of DooSelection in any other 
  * CMake project.
  *
- * @subsection mp_building Building DooCore
+ * @subsection mp_building Building DooSelection
  *
- * First set the environment variable @c $DOOCORESYS to the install location
+ * First set the environment variable @c $DOOSELECTIONSYS to the install location
  * as set above. If not installing into a standard location like @c /usr/local
  * or similar, you also have to set @c $LD_LIBRARY_PATH (on Linux) or 
- * @c $DYLD_LIBRARY_PATH (on Mac OS) to include @c $DOOCORESYS/lib. Best 
+ * @c $DYLD_LIBRARY_PATH (on Mac OS) to include @c $DOOSELECTIONSYS/lib. Best 
  * practice probably is to set these in your <tt>~/.bashrc</tt> or globally
  * (see remark about DooSoftware below to skip these steps).
  *
  * @code
- * export DOOCORESYS=/path/to/install/to
- * export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DOOCORESYS/lib
- * export PATH=$PATH:$DOOCORESYS/bin
+ * export DOOSELECTIONSYS=/path/to/install/to
+ * export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DOOSELECTIONSYS/lib
+ * export PATH=$PATH:$DOOSELECTIONSYS/bin
  * @endcode
  *
  * If setting in your <tt>~/.bashrc</tt>, remember to log out and in once or 
@@ -43,66 +42,63 @@
  * installation you can just use ~/LocalDooSoftware.sh, see 
  * https://wiki.e5.physik.tu-dortmund.de/LHCb/Software/DooSoftwareUsage).
  * 
- * To build DooCore, download it either as a release version or checkout from 
- * SVN. Create a @c build directory where you invoke CMake and install DooCore.
+ * To build DooSelection, download it either as a release version or checkout from 
+ * SVN. Create a @c build directory where you invoke CMake and install DooSelection.
  * 
  * @code
- * cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS /path/of/doocore/source
+ * cmake -DCMAKE_INSTALL_PREFIX=$DOOSELECTIONSYS /path/of/doocore/source
  * make
  * make install
  * @endcode
  *
  * 
- * @subsection mp_externalusage Using DooCore from an external project
+ * @subsection mp_externalusage Using DooSelection from an external project
  *
  * In the external project, adapt your <tt>CMakeLists.txt</tt> file to find 
- * DooCore and copy the provided <tt>FindDooCore.cmake</tt> (can be found in 
- * <tt>external/</tt> directory of DooCore) into your CMake modules path (which
+ * DooSelection and copy the provided <tt>FindDooSelection.cmake</tt> (can be found in 
+ * <tt>external/</tt> directory of DooSelection) into your CMake modules path (which
  * can be configured in CMakeLists.txt). The following example shows this 
  * procedure:
  *
  * @code
  * # Append local directory cmake/Modules to $CMAKE_MODULE_PATH.
- * # This is where you want to put the provided FindDooCore.cmake.
+ * # This is where you want to put the provided FindDooSelection.cmake.
  * set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
  * 
- * # Try to find DooCore (this requires $DOOCORESYS to be set)
- * find_package(DooCore)
+ * # Try to find DooSelection (this requires $DOOSELECTIONSYS to be set)
+ * find_package(DooSelection)
  * 
- * # Tell CMake to also use DooCore includes and libraries in its search path for
+ * # Tell CMake to also use DooSelection includes and libraries in its search path for
  * # includes and libraries.
- * include_directories(${DooCore_INCLUDE_DIR})
- * link_directories(${DooCore_LIBRARY_DIR})
+ * include_directories(${DooSelection_INCLUDE_DIR})
+ * link_directories(${DooSelection_LIBRARY_DIR})
  *
- * # Only if DooCore is found build this executable and link it.
+ * # Only if DooSelection is found build this executable and link it.
  * # this will protect the complete project from not compiling for anyone not 
- * # having DooCore installed. 
- * if (DooCore_FOUND)
+ * # having DooSelection installed. 
+ * if (DooSelection_FOUND)
  *   add_executable(MyBinary MainBinaryMain.cpp)
- *   target_link_libraries(MyBinary ${DooCore_LIBRARIES} ${MY_OTHER_LIBRARIES})
- * endif (DooCore_FOUND)
+ *   target_link_libraries(MyBinary ${DooSelection_LIBRARIES} ${MY_OTHER_LIBRARIES})
+ * endif (DooSelection_FOUND)
  * @endcode
  *
- * @section mp_usage How to use DooCore objects
+ * @section mp_usage How to use DooSelection objects
  *
- * Using DooCore from external projects is simple. DooCore is setup as a 
+ * Using DooSelection from external projects is simple. DooSelection is setup as a 
  * namespace with various sub-namespaces.
  * 
  * Take this example:
  *
  * @code
- * #include "doocore/lutils/lutils.h"
- * #include "doocore/io/MsgStream.h"
+ * #include "dooselection/reducer/Reducer.h"
  * 
  * int main(int argc, char* argv[]) {
  *   // the following lines might be handy and are only left out for 
  *   // demonstrational reasons
- *   // using namespace doocore::lutils;
- *   // using namespace doocore::io;
+ *   // using namespace dooselection::reducer;
  *   
- *   doocore::io::sinfo << "This is an info message in colour!" << doocore::io::endmsg;
- *   
- *   doocore::lutils::printPlot(...all the printPlot parameters go here...)
+ *   dooselection::reducer::Reducer myreducer;
+ *   myreducer.Run();
  * }
  * @endcode
  *
@@ -110,5 +106,5 @@
 
 /**
  * @namespace doocore
- * @brief Global DooCore namespace
+ * @brief Global DooSelection namespace
  */
