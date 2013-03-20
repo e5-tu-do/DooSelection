cc5417d808b639ecbb05b8ee96375bcdda58dde2
Christophe Cauet <christophe.cauet@tu-dortmund.de>
2013-03-20 08:21:52 +0000

    Added test files for possible veto class

---
+++ TestWiese/TestVeto/TestVeto.cpp
@@ -0,0 +1,13 @@
+#include "TMath.h"
+#include "TString.h"
+#include "TLorentzVector.h"
+
+#include "dooselection/reducer/Reducer.h"
+
+TLorentzVector lv(TString name, double mass){
+
+}
+
+int main(){
+
+}
