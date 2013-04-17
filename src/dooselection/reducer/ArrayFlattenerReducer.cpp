ec0e868dfafbc3c25683d64fea1633cab4e836fe
Florian Kruse <florian.kruse@tu-dortmund.de>
2013-04-17 21:00:32 +0200

    adding loops to ArrayFlattenerReducer::FillOutputTree() to fill flat leaves

--- src/dooselection/reducer/ArrayFlattenerReducer.cpp
+++ src/dooselection/reducer/ArrayFlattenerReducer.cpp
@@ -7,11 +7,15 @@
 // from RooFit
 
 // from DooCore
+#include "doocore/io/MsgStream.h"
 
 // from DooFit
 
 namespace dooselection {
 namespace reducer {
+
+using namespace doocore::io;
+  
 ArrayFlattenerReducer::ArrayFlattenerReducer()
   : leaf_array_length_(NULL)
 {}
@@ -21,18 +25,22 @@ void ArrayFlattenerReducer::CreateSpecialBranches() {
 }
   
 void ArrayFlattenerReducer::FillOutputTree() {
-  // loop over arrays and fill flat leaves
-
   int num_pvs = leaf_array_length_->GetValue();  
   for (int i=0; i<num_pvs; ++i) {
+    sdebug << "i = " << i << endmsg;
     for (std::map<ReducerLeaf<Double_t>*, const ReducerLeaf<Double_t>*>::iterator it = leaves_map_double_.begin();
          it != leaves_map_double_.end(); ++it) {
-      
+      (*it->first) = it->second->GetValue(i);
+      sdebug << " " << it->first->name() << " = " << it->first->GetValue() << endmsg;
+    }
+    for (std::map<ReducerLeaf<Int_t>*, const ReducerLeaf<Int_t>*>::iterator it = leaves_map_int_.begin();
+         it != leaves_map_int_.end(); ++it) {
+      (*it->first) = it->second->GetValue(i);
+      sdebug << " " << it->first->name() << " = " << it->first->GetValue() << endmsg;
     }
     
     FlushEvent();
   }
-
 }
 } // namespace reducer
 } // namespace dooselection
