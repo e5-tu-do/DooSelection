4682debd8a5bfc295b1429a377724f3c8a6d9a08
Florian Kruse <florian.kruse@tu-dortmund.de>
2014-02-05 15:34:29 +0100

    debugging with Frank

--- src/dooselection/reducer/Reducer.h
+++ src/dooselection/reducer/Reducer.h
@@ -842,9 +842,9 @@ class Reducer {
   void ActivateDependentLeaves(const std::vector<ReducerLeaf<T>* >& leaves) {
     using namespace doocore::io;
     for (auto leaf : leaves) {
-      sdebug << "leaf: " << leaf << endmsg;
-      sdebug << "leaf->leaf_pointer_one(): " << leaf->leaf_pointer_one() << endmsg;
-      sdebug << "leaf->leaf_pointer_one()->name(): " << leaf->leaf_pointer_one()->name() << endmsg;
+//      sdebug << "leaf: " << leaf << endmsg;
+//      sdebug << "leaf->leaf_pointer_one(): " << leaf->leaf_pointer_one() << endmsg;
+//      sdebug << "leaf->leaf_pointer_one()->name(): " << leaf->leaf_pointer_one()->name() << endmsg;
       if (leaf->leaf_pointer_one() != NULL) {
         if (input_tree_->GetLeaf(leaf->leaf_pointer_one()->name()) != NULL) {
           //sdebug << "Reactivating " << leaf->leaf_pointer_one()->name() << " (needed for operation in " << leaf->name() << ")" << endmsg;
