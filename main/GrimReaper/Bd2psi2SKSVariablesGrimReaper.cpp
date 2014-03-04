c2d47a59f41a2bb5adc95c07cf40f60796fde3f1
Christophe Cauet <christophe.cauet@tu-dortmund.de>
2014-03-04 16:26:19 +0100

    GrimReaper: HOTFIX: catched case where no DTF fit nor LOKI_DTF fit was performed and therefore no corresponding leaf is present.

--- main/GrimReaper/Bd2psi2SKSVariablesGrimReaper.cpp
+++ main/GrimReaper/Bd2psi2SKSVariablesGrimReaper.cpp
@@ -185,6 +185,12 @@ void MassLeaves(Reducer* _rdcr, cfg_tuple& cfg){
     ReducerLeaf<Double_t>& mass_loki_jpsi_pv_leaf     = _rdcr->CreateDoubleCopyLeaf("obsMassLokiJpsiPVConst", _rdcr->GetInterimLeafByName(std::get<0>(cfg)+"_LOKI_MASS_JpsiConstr"));
     ReducerLeaf<Double_t>& mass_err_loki_jpsi_pv_leaf = _rdcr->CreateDoubleCopyLeaf("obsMassErrLokiJpsiPVConst", _rdcr->GetInterimLeafByName(std::get<0>(cfg)+"_LOKI_MASSERR_JpsiConstr"));
   }
+  if (_rdcr->LeafExists(std::get<0>(cfg)+"_MM")) {
+    if (main_observable_constraint == "") main_observable_constraint = "MM";
+    if (main_observable_constraint_error == "") main_observable_constraint_error = "MMERR";
+    ReducerLeaf<Double_t>& mass_loki_jpsi_pv_leaf     = _rdcr->CreateDoubleCopyLeaf("obsMassLokiJpsiPVConst", _rdcr->GetInterimLeafByName(std::get<0>(cfg)+"_MM"));
+    ReducerLeaf<Double_t>& mass_err_loki_jpsi_pv_leaf = _rdcr->CreateDoubleCopyLeaf("obsMassErrLokiJpsiPVConst", _rdcr->GetInterimLeafByName(std::get<0>(cfg)+"_MMERR"));
+  }
   ReducerLeaf<Double_t>& mass_jpsi_ks_pv_leaf     = _rdcr->CreateDoubleCopyLeaf("obsMass", _rdcr->GetInterimLeafByName(std::get<0>(cfg)+"_"+main_observable_constraint+flat_suffix));
   ReducerLeaf<Double_t>& mass_err_jpsi_ks_pv_leaf = _rdcr->CreateDoubleCopyLeaf("obsMassErr", _rdcr->GetInterimLeafByName(std::get<0>(cfg)+"_"+main_observable_constraint_error+flat_suffix));
   summary.Add("obsMass fit constraints", main_observable_constraint);  
