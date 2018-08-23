eb746dd1b1cd065227d160f521f003e7415da51d
Alex Birnkraut <alex.birnkraut@tu-dortmund.de>
2018-08-23 09:25:07 +0200

    trying to get configs in doxygen working and added readme

--- doc/doxygen_doc.h
+++ doc/doxygen_doc.h
@@ -14,26 +14,26 @@
  * @li Find a list of pre-installed executables: @ref exes
  *
  * @section mp_simplesetup Simple setup via global DooSoftware installation
- * 
+ *
  * On all machines with a DooSoftware installation setup of DooSelection is very
- * simple. In this case you can skip the next building step. Just call 
+ * simple. In this case you can skip the next building step. Just call
  * dooSoftwareSetup after setting up LHCb software and DaVinci/ROOT and the
  * DooSoftware magic will do the rest.
- * 
+ *
  * @section mp_installation Manual Installation
  *
  * The intended usage of DooSelection is as a framework and therefore @a not to
- * build custom user code inside of DooSelection. However, DooSelection can be easily 
- * installed and interfaced from other external projects. FindDooSelection CMake 
- * modules are supplied which allow easy integration of DooSelection in any other 
+ * build custom user code inside of DooSelection. However, DooSelection can be easily
+ * installed and interfaced from other external projects. FindDooSelection CMake
+ * modules are supplied which allow easy integration of DooSelection in any other
  * CMake project.
  *
  * @subsection mp_building Building DooSelection
  *
  * First set the environment variable @c $DOOSELECTIONSYS to the install location
  * as set above. If not installing into a standard location like @c /usr/local
- * or similar, you also have to set @c $LD_LIBRARY_PATH (on Linux) or 
- * @c $DYLD_LIBRARY_PATH (on Mac OS) to include @c $DOOSELECTIONSYS/lib. Best 
+ * or similar, you also have to set @c $LD_LIBRARY_PATH (on Linux) or
+ * @c $DYLD_LIBRARY_PATH (on Mac OS) to include @c $DOOSELECTIONSYS/lib. Best
  * practice probably is to set these in your <tt>~/.bashrc</tt> or globally
  * (see remark about DooSoftware below to skip these steps).
  *
@@ -43,45 +43,45 @@
  * export PATH=$PATH:$DOOSELECTIONSYS/bin
  * @endcode
  *
- * If setting in your <tt>~/.bashrc</tt>, remember to log out and in once or 
+ * If setting in your <tt>~/.bashrc</tt>, remember to log out and in once or
  * copy the corresponding lines also into your terminal (in case of DooSoftware
- * installation you can just use ~/LocalDooSoftware.sh, see 
+ * installation you can just use ~/LocalDooSoftware.sh, see
  * https://wiki.e5.physik.tu-dortmund.de/LHCb/Software/DooSoftwareUsage).
- * 
- * To build DooSelection, download it either as a release version or checkout from 
+ *
+ * To build DooSelection, download it either as a release version or checkout from
  * Git. Create a @c build directory where you invoke CMake and install DooSelection.
- * 
+ *
  * @code
  * /path/of/dooselection/source/scripts/rebuild_cmake.sh
  * make
  * make install
  * @endcode
  *
- * 
+ *
  * @subsection mp_externalusage Using DooSelection from an external project
  *
- * In the external project, adapt your <tt>CMakeLists.txt</tt> file to find 
- * DooSelection and copy the provided <tt>FindDooSelection.cmake</tt> (can be found in 
+ * In the external project, adapt your <tt>CMakeLists.txt</tt> file to find
+ * DooSelection and copy the provided <tt>FindDooSelection.cmake</tt> (can be found in
  * <tt>external/</tt> directory of DooSelection) into your CMake modules path (which
- * can be configured in CMakeLists.txt). The following example shows this 
+ * can be configured in CMakeLists.txt). The following example shows this
  * procedure:
  *
  * @code
  * # Append local directory cmake/Modules to $CMAKE_MODULE_PATH.
  * # This is where you want to put the provided FindDooSelection.cmake.
  * set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
- * 
+ *
  * # Try to find DooSelection (this requires $DOOSELECTIONSYS to be set)
  * find_package(DooSelection)
- * 
+ *
  * # Tell CMake to also use DooSelection includes and libraries in its search path for
  * # includes and libraries.
  * include_directories(${DooSelection_INCLUDE_DIR})
  * link_directories(${DooSelection_LIBRARY_DIR})
  *
  * # Only if DooSelection is found build this executable and link it.
- * # this will protect the complete project from not compiling for anyone not 
- * # having DooSelection installed. 
+ * # this will protect the complete project from not compiling for anyone not
+ * # having DooSelection installed.
  * if (DooSelection_FOUND)
  *   add_executable(MyBinary MainBinaryMain.cpp)
  *   target_link_libraries(MyBinary ${DooSelection_LIBRARIES} ${MY_OTHER_LIBRARIES})
@@ -90,19 +90,19 @@
  *
  * @section mp_usage How to use DooSelection objects
  *
- * Using DooSelection from external projects is simple. DooSelection is setup as a 
+ * Using DooSelection from external projects is simple. DooSelection is setup as a
  * namespace with various sub-namespaces.
- * 
+ *
  * Take this example:
  *
  * @code
  * #include "dooselection/reducer/Reducer.h"
- * 
+ *
  * int main(int argc, char* argv[]) {
- *   // the following lines might be handy and are only left out for 
+ *   // the following lines might be handy and are only left out for
  *   // demonstrational reasons
  *   // using namespace dooselection::reducer;
- *   
+ *
  *   dooselection::reducer::Reducer myreducer;
  *   myreducer.Run();
  * }
@@ -119,41 +119,41 @@
  * @section exec_AddCategoryGrimReaper AddCategoryGrimReaper
  * The AddCategoryGrimReaper adds a new category leaf with the provided name
  * and sets the value to the specified integer.
- * 
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!), category_name, integer_value
  *
  * @section exec_ArrayFlattenerGrimReaper ArrayFlattenerGrimReaper
  * The ArrayFlattenerGrimReaper is able to flatten a tuple containing array-type
- * entries. 
- * 
+ * entries.
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!), config file name
- * 
+ *
  * See an example of a config file:
- * @verbinclude ArrayFlattenerGrimReaper.cfg
- * 
+ * @verbinclude examples/GrimReapger/ArrayFlattenerGrimReaper.cfg
+ *
  * @section exec_CandidateSelectionGrimReaper CandidateSelectionGrimReaper
- * The CandidateSelectionGrimReaper removes multiple candidates depending on a 
+ * The CandidateSelectionGrimReaper removes multiple candidates depending on a
  * given branch. The candidate with the smalles entry in the respective branch is
  * chosen.
- * 
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!), name of discriminating leaf.
- * 
+ *
  * @section exec_FlavourTaggingCalibrationGrimReaper FlavourTaggingCalibrationGrimReaper
  * The FlavourTaggingCalibrationGrimReaper performs a linear FT calibration for a
  * given tagging observable
- * 
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!), name of input tag observables,
  * name of input eta observable, name of input tagging category, p0 calibration parameters,
  * p1 calibration parameters, average eta calibration paramter, new observable name appendix.
- * 
+ *
  * @section exec_FitTupleGrimReaper FitTupleGrimReaper
  * The FitTupleGrimReaper removes all leaves not matching the following regular
  * expressions: "obs.*", "var.*", "cat.*", "par.*", "idx.*", and ".*_sw"
- * 
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!)
  *
@@ -166,12 +166,12 @@
  *
  * See an example of a config file:
  * @verbinclude MultiCutGrimReaper.cfg
- * 
+ *
  * @section exec_MultipleCandidateAnalyseGrimReaper MultipleCandidateAnalyseGrimReaper
  * The MultipleCandidateAnalyseGrimReaper analyses a given tuple with a view to
  * multiple candidates present in the tuple due to multiple PVs and B candidates.
- * 
- * Parameters needed: name of input file, name of input tree, name of output file, 
+ *
+ * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!)
  *
  * @section exec_RemoveSingleBranchGrimReaper RemoveSingleBranchGrimReaper
@@ -185,38 +185,38 @@
  *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree, cut string in quotation marks
- * 
+ *
  * @section exec_TMVAGrimReaper TMVAGrimReaper
- * The TMVAGrimReaper applies a TMVA trained MVA on a provided tuple and adds a 
+ * The TMVAGrimReaper applies a TMVA trained MVA on a provided tuple and adds a
  * branch containing the per-event classifier value.
- * 
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree (no backslashes!), config file name
- * 
+ *
  * See an example of a config file:
- * @verbinclude TMVAGrimReaper.cfg 
+ * @verbinclude TMVAGrimReaper.cfg
  *
  * @section exec_VariableCategorizerGrimReaper VariableCategorizerGrimReaper
  * The VariableCategorizerGrimReaper creates one or multiple new variables/categories
  * containing equally filled bins depending on a given distribution of an existing
- * variable. 
- * 
+ * variable.
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree, config file name
- * 
+ *
  * See an example of a config file:
  * @verbinclude VariableCategorizerGrimReaper.cfg
- * 
+ *
  * @section exec_VariableRenamerGrimReaper VariableRenamerGrimReaper
  * The VariableRenamerGrimReaper renames existing branches according to a provided
  * substitution
- * 
+ *
  * Parameters needed: name of input file, name of input tree, name of output file,
  * name of output tree, config file name
- * 
+ *
  * See an example of a config file:
  * @verbinclude VariableRenamerGrimReaper.cfg
- * 
+ *
  */
 
 /**
